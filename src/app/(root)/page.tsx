18368f56b8d94a05519b0259b6fe9675eaa8a218
Zakusei <zakusei.dev@gmail.com>
2024-05-25 03:02:02 +0800

    chore: change footer directory

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import { SiteHeader } from "@/components/layouts/site-header";
 import { HeroSection } from "./_components/hero-section";
 import { TrendingSection } from "./_components/trending-section";
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/layouts/footer";
 
 export default function Home() {
   return (
