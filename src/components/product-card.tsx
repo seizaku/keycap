7e77e766289465f9c77f67724f2affe92ab6bd80
Zakusei <zakusei.dev@gmail.com>
2024-05-24 16:07:45 +0800

    feat: add reusable product card component

---
+++ src/components/product-card.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export interface ProductCard {
+  title: string;
+  price: number;
+  artist: string;
+  src: string;
+}
+
+export const ProductCard = ({ title, price, artist, src }: ProductCard) => {
+  return (
+    <Link href="" className="h-96 w-full">
+      <div className="relative h-full w-full bg-muted flex justify-center items-center">
+        <Image fill alt="image" className="object-cover" src={src} />
+      </div>
+      <div className="mt-2">
+        <span className="font-medium">{title}</span>
+        <div className="flex justify-between items-center">
+          <span className="text-muted-foreground">By {artist}</span>
+          <span className="font-bold">${price}</span>
+        </div>
+      </div>
+    </Link>
+  );
+};
