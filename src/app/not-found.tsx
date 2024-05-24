06b70a7017b25bfe4e353b5c677049df8075e94b
Zakusei <zakusei.dev@gmail.com>
2024-05-25 02:51:50 +0800

    feat: change 404 page text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,22 @@
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-
+import { SiteHeader } from "@/components/layouts/site-header";
+import { Footer } from "@/components/layouts/footer";
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="container mx-auto flex flex-col justify-center items-center h-screen">
+    <main className="h-screen">
+      <SiteHeader />
+      <div className="container mx-auto flex flex-col justify-center items-center h-full">
         <h1 className="text-8xl font-bold">404</h1>
         <h2 className="text-4xl font-medium">{`Something's Missing`}</h2>
         <p className="text-lg text-muted-foreground mt-4 text-center">
-          Sorry, we can't find that page. You'll find lots to explore on the
-          home page.
+          This page might still be under construction.
         </p>
         <Link href={"/"} className={buttonVariants({ className: "mt-4" })}>
           Back to Homepage
         </Link>
       </div>
+      <Footer />
     </main>
   );
 }
