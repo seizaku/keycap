0732b5237445a5eef3177f8cf3ced4aae7435331
Zakusei <zakusei.dev@gmail.com>
2024-05-25 03:01:05 +0800

    feat: add shop accordion to mobile

--- src/components/layouts/mobile-nav.tsx
+++ src/components/layouts/mobile-nav.tsx
@@ -37,10 +37,49 @@ export const MobileNav = ({ items }: SideNavProps) => {
       <SheetContent side={"left"} className="text-sm">
         <SheetHeader>
           <SheetTitle>
-            <BrandLogo className="h-8 w-fit" />
+            <BrandLogo className="h-8 w-fit fill-background" />
           </SheetTitle>
         </SheetHeader>
         <Accordion type="single" collapsible className="w-full mt-4">
+          <AccordionItem value={`item-0`}>
+            <AccordionTrigger>Shop</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                <li className="py-1.5">
+                  <Link
+                    className="pl-2 text-muted-foreground hover:text-foreground"
+                    href={`/`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="py-1.5">
+                  <Link
+                    className="pl-2 text-muted-foreground hover:text-foreground"
+                    href={`/products`}
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li className="py-1.5">
+                  <Link
+                    className="pl-2 text-muted-foreground hover:text-foreground"
+                    href={`/custom`}
+                  >
+                    Custom Keycap
+                  </Link>
+                </li>
+                <li className="py-1.5">
+                  <Link
+                    className="pl-2 text-muted-foreground hover:text-foreground"
+                    href={`/blog`}
+                  >
+                    Blog
+                  </Link>
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
           {items?.map((item, index) => {
             if (!item.items) {
               return (
