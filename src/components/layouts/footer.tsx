a8ccbfb63aee286dba1ba6ee7e90bc2aa6e1af5a
Zakusei <zakusei.dev@gmail.com>
2024-05-25 03:01:41 +0800

    style: responsive footer component

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -11,37 +11,39 @@ export const Footer = () => {
   return (
     <footer className="py-6 border-t">
       <div className="container">
-        <div className="flex justify-between gap-24">
-          <BrandLogo className="h-8 w-fit fill-background" />
-          {siteConfig.footer.map((footer_item) => {
-            return (
-              <ul
-                key={footer_item.category}
-                className="flex flex-col gap-4 text-muted-foreground"
-              >
-                <li className="font-medium text-foreground">
-                  {footer_item.category}
-                </li>
-                {footer_item.items.map((item) => {
-                  return (
-                    <li key={item.title}>
-                      <a
-                        href={`${item.href}`}
-                        className="hover:text-foreground"
-                      >
-                        {item.title}
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-            );
-          })}
+        <div className="flex flex-col lg:flex-row justify-between gap-24">
+          <BrandLogo className="h-8 w-fit fill-background col-span-3 lg:col-span-1" />
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-24 justify-between">
+            {siteConfig.footer.map((footer_item) => {
+              return (
+                <ul
+                  key={footer_item.category}
+                  className="flex flex-col gap-4 text-muted-foreground"
+                >
+                  <li className="font-medium text-foreground">
+                    {footer_item.category}
+                  </li>
+                  {footer_item.items.map((item) => {
+                    return (
+                      <li key={item.title}>
+                        <a
+                          href={`${item.href}`}
+                          className="hover:text-foreground"
+                        >
+                          {item.title}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              );
+            })}
+          </div>
           <form action="">
             <p className="text-lg font-medium mb-4">
               Subscribe to our newsletter
             </p>
-            <div className="relative">
+            <div className="relative w-fit">
               <Input
                 type="email"
                 placeholder="seizaku.dev@gmail.com"
