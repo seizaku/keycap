88a95848d12fe1edcb733474e25e4d508fe2eea9
Zakusei <zakusei.dev@gmail.com>
2024-06-01 01:33:02 +0800

    feat: signin form

--- src/app/(auth)/signin/_components/signin-form.tsx
+++ src/app/(auth)/signin/_components/signin-form.tsx
@@ -1,16 +1,54 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Label } from "@/components/ui/label";
+import { schema } from "@/schema/email-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { signInWithEmail } from "@/actions/auth";
+import { SubmitButton } from "@/components/submit-button";
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
 
 export const SignInForm = () => {
+  const form = useForm<z.output<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function onSubmit(form: z.output<typeof schema>) {
+    const formData = new FormData();
+    formData.append("email", form.email);
+    console.log(await signInWithEmail(formData));
+  }
+
   return (
-    <form action="" className="w-full space-y-4">
-      <div className="grid gap-1.5">
-        <Label>Email</Label>
-        <Input type="email" placeholder="seizaku.dev@gmail.com" />
-      </div>
-      <Button className="w-full">Sign in</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="seizaku.dev@gmail.com" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <SubmitButton className="w-full">Sign in</SubmitButton>
+      </form>
+    </Form>
   );
 };
